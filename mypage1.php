2ab2a7a89d095e02a6493db7a6d32e5ac51335ed
Patteera09 <97584551+Patteera09@users.noreply.github.com>
2022-03-02 13:24:32 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>เว็บไซต์ของพัทธ์ธีรา</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"><h1>Facebook</h1></a>
+    <a href="mypage2.php"><h1>Mypage2</h1></a>
+  </body>
+</html>
