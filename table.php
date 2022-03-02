968256e087cb858bc77da3630a66d98fc305fc83
Patteera09 <97584551+Patteera09@users.noreply.github.com>
2022-03-02 22:52:18 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/Instragram</th>
+      </tr>
+      <tr>
+        <td>1</td>                  
+        <td>Goki Maeda</td>
+        <td><img src="https://high-low.fandom.com/wiki/Todoroki_Yosuke=.jpg" width="45px"></td>
+        <td><a href="https://instagram.com/maeda.gouki?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Park Jimin</td>
+        <td><img src="https://images.app.goo.gl/LwEkD7FKyRsiLx8u9" width="45px"></td>
+        <td><a href="https://instagram.com/j.m?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>ariana grande</td>
+        <td><img src="https://images.app.goo.gl/Vk8nRRXXrBECJAra7" width="45px"></td>
+        <td><a href="https://instagram.com/arianagrande?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>The Weeknd</td>
+        <td><img src="https://images.app.goo.gl/We5b6CWEDa4AzoRH6" width="45px"></td>
+        <td><a href="https://instagram.com/theweeknd?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Lại Quán Lâm</td>
+        <td><img src="https://images.app.goo.gl/5oiY6dArAvAJW7p16" width="45px"></td>
+        <td><a href="https://instagram.com/official_lai_kuanlin?utm_medium=copy_link">Instragram</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
