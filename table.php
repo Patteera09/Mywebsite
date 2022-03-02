39721de78896a6a631e32484e90059d937205533
Patteera09 <97584551+Patteera09@users.noreply.github.com>
2022-03-02 23:12:04 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,31 +13,31 @@
       <tr>
         <td>1</td>                  
         <td>Goki Maeda</td>
-        <td><img src="https://static.wikia.nocookie.net/high-low/images/5/58/Todoroki_Yosuke.jpg/revision/latest?cb=20211116134610" width="45px"></td>
+        <td><img src="https://asianwiki.com/File:Goki_Maeda-p02.jpg" width="45px"></td>
         <td><a href="https://instagram.com/maeda.gouki?utm_medium=copy_link">Instragram</a></td>
       </tr>
       <tr>
         <td>2</td>
         <td>Park Jimin</td>
-        <td><img src="https://th.m.wikipedia.org/wiki/%E0%B9%84%E0%B8%9F%E0%B8%A5%E0%B9%8C:190226_Jimin_at_the_6th_EDaily_Culture_Awards.jpg" width="45px"></td>
+        <td><img src="https://en.m.wikipedia.org/wiki/File:Jimin_x_Samsung_Galaxy_August_2021.png" width="45px"></td>
         <td><a href="https://instagram.com/j.m?utm_medium=copy_link">Instragram</a></td>
       </tr>
       <tr>
         <td>3</td>
         <td>ariana grande</td>
-        <td><img src="https://en.m.wikipedia.org/wiki/File:Ariana_Grande_Grammys_Red_Carpet_2020.png" width="45px"></td>
+        <td><img src="https://cms.thaiticketmajor.com/imgUpload/imgeditor/unnamed(4).jpg" width="45px"></td>
         <td><a href="https://instagram.com/arianagrande?utm_medium=copy_link">Instragram</a></td>
       </tr>
       <tr>
         <td>4</td>
         <td>The Weeknd</td>
-        <td><img src="https://en.m.wikipedia.org/wiki/File:FEQ_July_2018_The_Weeknd_(44778856382)_(cropped).jpg" width="45px"></td>
+        <td><img src="https://www.theweeknd.com/sites/g/files/aaj14496/f/styles/suzuki_breakpoints_image_tablet_16x9/public/promo/202201/mobile-banner.jpg?itok=bfYVN4wZ" width="45px"></td>
         <td><a href="https://instagram.com/theweeknd?utm_medium=copy_link">Instragram</a></td>
       </tr>
       <tr>
         <td>5</td>
         <td>Lại Quán Lâm</td>
-        <td><img src="https://en.m.wikipedia.org/wiki/File:Lai_kuanlin_at_shanghai_event.jpg" width="45px"></td>
+        <td><img src="https://i.mydramalist.com/EZByQ_5c.jpg" width="45px"></td>
         <td><a href="https://instagram.com/official_lai_kuanlin?utm_medium=copy_link">Instragram</a></td>
       </tr>
     </table>
