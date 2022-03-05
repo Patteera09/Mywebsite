bfbb814602140ac715a93277bbb42415ff09d948
Patteera09 <97584551+Patteera09@users.noreply.github.com>
2022-03-05 12:13:27 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("Chocolates .ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("MiPancake.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family:  "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวพัทธ์ธีรา คำมั่น เลขที่ 22 ห้อง ม.4/12 เลขประจำตัว 57068</h1>
+    <h1 id="font2">นางสาวพัทธ์ธีรา คำมั่น เลขที่ 22 ห้อง ม.4/12 เลขประจำตัว 57068</h1> 
+  </body>
+</html>
